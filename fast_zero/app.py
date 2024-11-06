1283cd3b573f79b36cf714dfa6bfb9bf87ead79a
Everson Esteves <119083241+esteveseverson@users.noreply.github.com>
2024-11-06 10:36:38 -0300

    fake_db_crud

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,12 +1,58 @@
 from http import HTTPStatus
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
-from fast_zero.schemas import Message
+from fast_zero.schemas import Message, UserDB, UserList, UserPublic, UserSchema
 
 app = FastAPI()
+
+fake_db = []
 
 
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá Mundo'}
+
+
+@app.get('/users/', response_model=UserList)
+def read_users():
+    return {'users': fake_db}
+
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    user_with_id = UserDB(
+        id=len(fake_db) + 1,
+        **user.model_dump(),  # transforma do pydentic para um dicionario
+    )
+
+    fake_db.append(user_with_id)
+
+    return user_with_id
+
+
+@app.put('/users/{user_id}', response_model=UserPublic)
+def update_user(user_id: int, user: UserSchema):
+    user_with_id = UserDB(id=user_id, **user.model_dump())
+
+    # validando alcance do user_id
+    if user_id < 1 or user_id > len(fake_db):
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='USER NOT FOUND'
+        )
+
+    fake_db[user_id - 1] = user_with_id
+
+    return user_with_id
+
+
+@app.delete('/users/{user_id}', response_model=Message)
+def delete_user(user_id: int):
+    if user_id < 1 or user_id > len(fake_db):
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='USER NOT FOUND'
+        )
+
+    del fake_db[user_id - 1]
+
+    return {'message': 'User deleted!'}
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,14 +1,84 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from fast_zero.app import app
-
-
-def test_read_root_deve_retornar_ok_e_ola_mundo():
-    client = TestClient(app)  # arrange
-
+def test_read_root_deve_retornar_ok_e_ola_mundo(client):
     response = client.get('/')  # act
 
     assert response.status_code == HTTPStatus.OK  # assert
     assert response.json() == {'message': 'Olá Mundo'}
+
+
+def test_create_user(client):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'testusername',
+            'password': 'password',
+            'email': 'test@test.com',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'id': 1,
+        'username': 'testusername',
+        'email': 'test@test.com',
+    }
+
+
+def test_read_users(client):
+    response = client.get('/users/')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'users': [
+            {
+                'id': 1,
+                'username': 'testusername',
+                'email': 'test@test.com',
+            }
+        ]
+    }
+
+
+def test_update_user(client):
+    response = client.put(
+        '/users/1',
+        json={
+            'id': 1,
+            'username': 'testusername2',
+            'password': '123',
+            'email': 'test@test.com',
+        },
+    )
+    assert response.json() == {
+        'id': 1,
+        'username': 'testusername2',
+        'email': 'test@test.com',
+    }
+
+
+def test_update_user_inexistente(client):
+    response = client.put(
+        '/users/2',
+        json={
+            'id': 2,
+            'username': 'testusername3',
+            'password': '123',
+            'email': 'test@test.com',
+        },
+    )
+    assert response.json() == {'detail': 'USER NOT FOUND'}
+
+
+def test_delete_user(client):
+    response = client.delete('/users/1')
+
+    assert response.json() == {'message': 'User deleted!'}
+
+
+def test_delete_user_inexistente(client):
+    response = client.delete(
+        '/users/2',
+    )
+    assert response.json() == {'detail': 'USER NOT FOUND'}
